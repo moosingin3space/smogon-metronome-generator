54221bf85e267b5fe900562ae7e90f62ce664fb1
Andrew Yang <antongyang@yahoo.com>
2021-10-26 18:57:35 -0600

    squashed

--- src/pokemon/heldItem.ts
+++ src/pokemon/heldItem.ts
@@ -1,16 +1,4 @@
 export const AllItems = [
-    //'Choice Band',
-    //'Choice Scarf',
-    //'Choice Specs',
-    //'Eviolite',
-    //'Expert Belt',
-    //'Focus Sash',
-    //'Heavy-Duty Boots',
-    //'Life Orb',
-    //'Mental Herb',
-    //'Power Herb',
-    //'Salac Berry',
-    // TODO
     'Absolite',
     'Absorb Bulb',
     'Adrenaline Orb',
